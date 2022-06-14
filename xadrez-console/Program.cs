617c1d602b15319d64dbb88a5f869137973e9975
Mateus-S <mateus.gcr.tog@hotmail.com>
2022-06-13 23:32:37 -0300

    Pecas e Tabuleiros implementadas ainda falta regras de negocio e moviementacao das pecas

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using xadrez_console;
+using xadrez_console.tabuleiro;
+using xadrez_console.Xadrex;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main( string [] args)
+        {
+            try
+            {
+                PartidaDeXadrex partida = new PartidaDeXadrex();
+
+                Tela.imprimirTabuleiro(partida.tab);
+            }
+            catch (TabuleiroException e )
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xadrez-console/Xadrex/Rei.cs
@@ -0,0 +1,17 @@
+﻿using xadrez_console;
+
+namespace xadrez_console.Xadrex
+{
+    internal class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
